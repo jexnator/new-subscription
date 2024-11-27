1541d25cd752dd312447f797bad26721549477ef
Xavier Jeiziner <83689318+jexnator@users.noreply.github.com>
2024-11-27 03:45:23 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,11 +11,11 @@
 terraform {
   required_version = ">= 1.9.8"
   backend "azurerm" {
-    subscription_id      = var.subscription_id
-    resource_group_name  = var.backend_resource_group_name
-    storage_account_name = var.backend_storage_account_name
-    container_name       = var.backend_container_name
-    key                  = var.backend_key
+    subscription_id      = "c60aafda-2e3a-4320-a98a-0e1e2466e042"
+    resource_group_name  = "dev-ressgr-terraform"
+    storage_account_name = "pocmarti"
+    container_name       = "terraform-state"
+    key                  = "dev-terraform.tfstate"
   }
 }
 
